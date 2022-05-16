97af85abc2a442915140f986b9fb1c4467f2c966
ibsenriou <ibsenriou@hotmail.com>
2022-05-16 10:24:26 -0300

    make functional tests an app, use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,12 +1,13 @@
+from django.test import LiveServerTestCase
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 import time
-import unittest
 
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -23,7 +24,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith ouviu falar de uma nova aplicação online interessante para
         # lista de tarefas. Ela decide verificar sua homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # Ela percebe que o título da página e o cabeçalho mencionam listas de 
         # tarefas (to-do)
@@ -73,6 +74,3 @@ class NewVisitorTest(unittest.TestCase):
 
 
 # Satisfeita, ela volta a dormir
-
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
