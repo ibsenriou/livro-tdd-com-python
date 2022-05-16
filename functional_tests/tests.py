c4f895e11958669dc9c806daa82e1035a90da07f
ibsenriou <ibsenriou@hotmail.com>
2022-05-16 19:39:09 -0300

    first steps of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -14,9 +14,12 @@ class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
+        time.sleep(1)
 
     def tearDown(self):
+        time.sleep(1)
         self.browser.quit()
+        time.sleep(1)
 
     def wait_for_row_in_list_table(self, row_text):
         start_time = time.time()
@@ -72,7 +75,6 @@ class NewVisitorTest(LiveServerTestCase):
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
         # Satisfeita, ela volta a dormir
-        time.sleep(5)
 
     def test_multiple_users_ca_start_lists_at_different_urls(self):
         # Edith inicia uma nova lista de tarefas
@@ -119,4 +121,28 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('Buy milk', page_text)
 
         # Satisfeitos, ambos voltam a dormir
-        time.sleep(3)
+
+    def test_layout_and_styling(self):
+        # Edit acessa a página inicial
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # Ela percebe que a caixa de entrada está elegantemente centralizada
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
+        # Ela inicia uma nova lista e vê que a entrada está elegantemente
+        # centralizada aí também
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
