4dc0828455f678e8d83ec40a950416369fe38030
Ibsen Gabriel Santos <ibsenriou@hotmail.com>
2022-05-23 20:09:25 -0300

    Moved Fts into their own individual files

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -1,42 +1,11 @@
-import os
-
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-
+from .base import FunctionalTest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import WebDriverException
-
-import time
 
 
-MAX_WAIT = 10
+class NewVisitorTest(FunctionalTest):
 
-
-class NewVisitorTest(StaticLiveServerTestCase):
-
-    def setUp(self):
-        self.browser = webdriver.Firefox()
-        staging_server = os.environ.get('STAGING_SERVER')
-        if staging_server:
-            self.live_server_url = 'http://' + staging_server
-
-    def tearDown(self):
-        self.browser.quit()
-
-    def wait_for_row_in_list_table(self, row_text):
-        start_time = time.time()
-        while True:
-            try:
-                table = self.browser.find_element_by_id('id_list_table')
-                rows = table.find_elements_by_tag_name('tr')
-                self.assertIn(row_text, [row.text for row in rows])
-                return
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT:
-                    raise e
-                time.sleep(0.5)
-
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def test_can_start_a_list_for_one_user(self):
         # Edith ouviu falar de uma nova aplicação online interessante para
         # lista de tarefas. Ela decide verificar sua homepage
         self.browser.get(self.live_server_url)
@@ -78,7 +47,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
         # Satisfeita, ela volta a dormir
 
-    def test_multiple_users_ca_start_lists_at_different_urls(self):
+    def test_multiple_users_can_start_lists_at_different_urls(self):
         # Edith inicia uma nova lista de tarefas
         self.browser.get(self.live_server_url)
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -94,7 +63,6 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
         ## Usamos uma nova sessão do navegador para garantir que nenhuma informação
         ## de Edith está vindo de cookies etc
-        time.sleep(3)
         self.browser.quit()
 
         self.browser = webdriver.Firefox()
@@ -123,28 +91,3 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.assertIn('Buy milk', page_text)
 
         # Satisfeitos, ambos voltam a dormir
-
-    def test_layout_and_styling(self):
-        # Edit acessa a página inicial
-        self.browser.get(self.live_server_url)
-        self.browser.set_window_size(1024, 768)
-
-        # Ela percebe que a caixa de entrada está elegantemente centralizada
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2,
-            512,
-            delta=10
-        )
-
-        # Ela inicia uma nova lista e vê que a entrada está elegantemente
-        # centralizada aí também
-        inputbox.send_keys('testing')
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: testing')
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2,
-            512,
-            delta=10
-        )
