8b5a080143057044bb1bdd65210f52d36d2bf88c
Ibsen Gabriel Santos <ibsenriou@hotmail.com>
2022-05-25 16:18:56 -0300

    Conclusão do Capítulo 15

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,10 +1,10 @@
-from unittest import skip
-
 from django.test import TestCase
 from django.utils.html import escape
 
+from ..forms import (
+    EMPTY_ITEM_ERROR, ItemForm, ExistingListItemForm
+)
 from ..models import Item, List
-from ..forms import ItemForm, EMPTY_ITEM_ERROR
 
 
 class HomePageTest(TestCase):
@@ -86,10 +86,9 @@ class ListViewTest(TestCase):
     def test_display_item_form(self):
         list_ = List.objects.create()
         response = self.client.get(f'/lists/{list_.id}/')
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
         self.assertContains(response, 'name="text"')
 
-    @skip
     def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
         list1 = List.objects.create()
         item1 = Item.objects.create(list=list1, text='textey')
@@ -162,3 +161,7 @@ class NewListTest(TestCase):
     def test_for_invalid_input_shows_error_on_page(self):
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
+
+    def test_for_invalid_input_passes_from_to_template(self):
+        response = self.post_invalid_input()
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
